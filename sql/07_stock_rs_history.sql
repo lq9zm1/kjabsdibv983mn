8dd05e1e1b78ad33bbf42ac9ef31f53c525a8009
alexway66 <alexway.business@gmail.com>
2026-06-09 14:54:41 -0500

    Update 07_stock_rs_history.sql

--- sql/07_stock_rs_history.sql
+++ sql/07_stock_rs_history.sql
@@ -3,14 +3,14 @@ PARTITION BY date AS
 WITH spy AS (
   SELECT date, adj_close AS spy_close
   FROM `stonks-498420.stonks_data.price_history`
-  WHERE date >= '2025-06-01' AND ticker='SPY'
+  WHERE date >= '2025-06-01' AND date < CURRENT_DATE() AND ticker='SPY'
 ),
 joined AS (
   SELECT p.ticker, p.date,
     p.adj_close / NULLIF(s.spy_close,0) AS rs_ratio
   FROM `stonks-498420.stonks_data.price_history` p
   JOIN spy s USING (date)
-  WHERE p.date >= '2025-06-01'
+  WHERE p.date >= '2025-06-01' AND p.date < CURRENT_DATE()
 )
 SELECT ticker, date, ROUND(rs_ratio,6) AS rs_ratio
 FROM joined;
